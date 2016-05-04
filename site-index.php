393bbeeee90c481ebb0048d83f53baa6b5fca39f
Jonathan Vollebregt <jnvsor@gmail.com>
2016-05-04 20:17:12 +0200

    Update addsite/delsite/site-index

--- site-index.php
+++ site-index.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Local Site Index</title>
+        <style>
+        body {
+            margin:40px auto;
+            max-width: 650px;
+            line-height: 1.6;
+            font-size: 18px;
+            color: #444;
+            padding: 0 10px;
+        }
+
+        h1, h2, h3 {
+            line-height: 1.2;
+        }
+
+        a {
+            color: #47A;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #8AC;
+        }
+        </style>
+    </head>
 <!---
 Localhost index to list sites created with addsite and delsite
 -->
-    <head>
-        <title>Local Site Index</title>
-    </head>
     <body>
         <h1>Local Site Index</h1>
         <ul>
+            <li><a href="/adminer/">Adminer</a></li>
 <?php
 
 foreach (scandir($_SERVER['DOCUMENT_ROOT']) as $entry)
